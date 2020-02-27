2b86f3dbf840dced59526a486ed4c134ac673be8
IvanPostu <ipostu20000127@gmail.com>
2020-02-27 13:37:40 -0500

    added css pre processor less and sass, initial babel configuration

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,11 @@
 import { app } from '@App/App';
 import './global.css';
+import './less.less';
+import './scss.scss';
 import testjson from '../testfiles/test.json';
 import testxml from '../testfiles/test.xml'
 import img from '../testfiles/beach-sea-coast-water-sand-rock-58319-pxhere.com.jpg';
+import './asyncTest'
 
 // document.addEventListener('click', function () {
 //   window.confirm('A you ready???')
@@ -11,4 +14,4 @@ import img from '../testfiles/beach-sea-coast-water-sand-rock-58319-pxhere.com.j
 // console.log('JSON:', testjson);
 // console.log('XML:', testxml);
 
-app(1)
+// app(1)
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,141 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: './main/index.js'
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js', '.jpg'],//чтобы в ручную в импортах не писать .exten
-    alias: {
-      '@App': path.resolve(__dirname, 'src/App'),
-      '@': path.resolve(__dirname, 'src'),
-    }
-  },
-  optimization: {
+function optimization(isDev, isProd) {
+  const config = {
     splitChunks: {
       chunks: 'all'
     }
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './main/index.html'
-    }),
-    new CleanWebpackPlugin()
+  }
 
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-
-      }, {
-        test: /\.(png|jpg|svg|gif)$/,
-        use: ['file-loader']
-      }, {
-        test: /\.(ttf|woff|woff2|eot)$/,
-        use: ['file-loader']
-      }, {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      }
-
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
     ]
-  },
+  }
 
 
+  return config
+}
+
+function cssLoaders(isDev, isProd, extraLoader) {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      }
+    }, 'css-loader'
+  ]
+
+  if (extraLoader) {
+    loaders.push(extraLoader)
+  }
+
+  return loaders
+}
 
 
+module.exports = (env, options) => {
 
+  const isDev = options.mode === 'development'
+  const isProd = !isDev
+
+  console.log('Production: ', isProd)
+  console.log('Development: ', isDev)
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['@babel/polyfill', './main/index.js']
+    },
+    output: {
+      filename: isProd ? '[name].[contenthash].js' : '[name].[hash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      port: 8081,
+      hot: isDev
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['.js', '.jpg'],//чтобы в ручную в импортах не писать .exten
+      alias: {
+        '@App': path.resolve(__dirname, 'src/App'),
+        '@': path.resolve(__dirname, 'src'),
+      }
+    },
+    optimization: optimization(isDev, isProd),
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: './main/index.html',
+        minify: {
+          collapseWhitespace: isProd
+        }
+      }),
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]),
+      new MiniCssExtractPlugin({
+        filename: isProd ? '[name].[contenthash].css' : '[name].[hash].css'
+      })
+
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          },
+
+        },
+        {
+          test: /\.css$/,
+          use: cssLoaders(isDev, isProd)
+
+        },
+        {
+          test: /\.less$/,
+          use: cssLoaders(isDev, isProd, 'less-loader')
+
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: cssLoaders(isDev, isProd, 'sass-loader')
+
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          use: ['file-loader']
+        }, {
+          test: /\.(ttf|woff|woff2|eot)$/,
+          use: ['file-loader']
+        }, {
+          test: /\.xml$/,
+          use: ['xml-loader']
+        }
+
+      ]
+    }
+  }
 }
 
 
