03af271f83f867219efaac40d17164e502f9ad8f
IvanPostu <ipostu20000127@gmail.com>
2020-04-07 02:55:01 -0400

    bug fixed

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,14 +1,15 @@
 // import { app } from '@App/App';
-import './global.css';
-import './less.less';
-import './scss.scss';
+import React from 'react';
+import { render } from 'react-dom';
 // import testjson from '../testfiles/test.json';
 // import testxml from '../testfiles/test.xml'
 // import img from '../testfiles/beach-sea-coast-water-sand-rock-58319-pxhere.com.jpg';
-import './asyncTest'
-import test from './ts.ts'
-import React from 'react'
-import { render } from 'react-dom'
+import './asyncTest';
+import './global.css';
+import './less.less';
+import './scss.scss';
+import test from './ts.ts';
+import * as util from '@/main/util.js'
 
 // document.addEventListener('click', function () {
 //   window.confirm('A you ready???')
@@ -16,13 +17,18 @@ import { render } from 'react-dom'
 
 // console.log('JSON:', testjson);
 // console.log('XML:', testxml);
-
+util.sayGoodbye('ADFADF')
 // app(1)
+// util.sayHello('aa')
+
+
+
+
 test()
 
 const App = () => (
   <div>
-    Hey !!!
+    zHey !!!
   </div>
 )
 
---
+++ src/main/util.js
@@ -0,0 +1,12 @@
+
+
+
+
+export function sayHello(s) {
+  console.log('hello ', s)
+}
+
+
+export function sayGoodbye(s) {
+  console.log('goodbye' + ` ${s}`)
+}
